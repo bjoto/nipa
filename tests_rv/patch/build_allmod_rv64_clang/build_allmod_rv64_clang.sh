6a30476eba26e4f30a63625ed328ef0b27db7345
Björn Töpel <bjorn@rivosinc.com>
2022-10-29 13:27:14 +0100

    rv: add a clang allmodconfig test
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>
    [cherry picked and dropped the podman stuff]
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

---
+++ tests_rv/patch/build_allmod_rv64_clang/build_allmod_rv64_clang.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (c) 2022 by Rivos Inc.
+
+tmpdir=$(mktemp -d)
+rc=0
+
+tuxmake --wrapper ccache --target-arch riscv --directory . \
+	--environment=KBUILD_BUILD_TIMESTAMP=@1621270510 \
+	--environment=KBUILD_BUILD_USER=tuxmake --environment=KBUILD_BUILD_HOST=tuxmake \
+	-o $tmpdir --toolchain llvm allmodconfig || rc=1
+
+if [ $rc -ne 0 ]; then
+  echo "Build failed" >&$DESC_FD
+fi
+
+rm -rf $tmpdir
+
+exit $rc
