19b4a030a5b631630151d6f047fd3569a903628e
Conor Dooley <conor.dooley@microchip.com>
2022-11-04 13:27:05 +0000

    rv: s/llvm-nightly/llvm
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- tests_rv/patch/build_rv64_clang_allmodconfig/build_rv64_clang_allmodconfig.sh
+++ tests_rv/patch/build_rv64_clang_allmodconfig/build_rv64_clang_allmodconfig.sh
@@ -9,7 +9,7 @@ rc=0
 tuxmake --wrapper ccache --target-arch riscv --runtime podman --directory . \
         --environment=KBUILD_BUILD_TIMESTAMP=@1621270510 \
         --environment=KBUILD_BUILD_USER=tuxmake --environment=KBUILD_BUILD_HOST=tuxmake \
-        -o $tmpdir --toolchain llvm-nightly -z none --kconfig allmodconfig \
+        -o $tmpdir --toolchain llvm -z none --kconfig allmodconfig \
         -K CONFIG_RANDSTRUCT_NONE=y || rc=1
 
 if [ $rc -ne 0 ]; then
