65b47f95d149906b4f80d3eacd29ccadfbc15f8d
Conor Dooley <conor.dooley@microchip.com>
2022-12-02 13:15:44 +0000

    rv: add RMK's script for select ordering
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

---
+++ tests_rv/patch/alphanumeric_selects/alphanumeric_selects.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+#
+# composite series of images over a background image
+#
+#
+while (<>) {
+        while (/\\\s*$/) {
+                $_ .= <>;
+        }
+        undef %selects if /^\s*config\s+/;
+        if (/^\s+select\s+(\w+).*/) {
+                if (defined($selects{$1})) {
+                        if ($selects{$1} eq $_) {
+                                print STDERR "Warning: removing duplicated $1 entry\n";
+                        } else {
+                                print STDERR "Error: $1 differently selected\n".
+                                        "\tOld: $selects{$1}\n".
+                                        "\tNew: $_\n";
+                                exit 1;
+                        }
+                }
+                $selects{$1} = $_;
+                next;
+        }
+        if (%selects and (/^\s*$/ or /^\s+help/ or /^\s+---help---/ or
+                          /^endif/ or /^endchoice/)) {
+                foreach $k (sort (keys %selects)) {
+                        print "$selects{$k}";
+                }
+                undef %selects;
+        }
+        print;
+}
+if (%selects) {
+        foreach $k (sort (keys %selects)) {
+                print "$selects{$k}";
+        }
+}
